b1ea4366e82497c2bb9ea937cd6c0f84633cc86a
Dmytro Lykholit <lixolitr@gmail.com>
2016-12-19 22:01:54 +0200

    //add commentary for testing (create new branch)

--- src/main/java/com/calculator/CalculatorController.java
+++ src/main/java/com/calculator/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // public class CalculatorController
+// test commentary for creating new branch on github with idea
 @RestController
 public class CalculatorController {
 
